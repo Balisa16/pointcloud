2e5c397ec2af35bd45f7e7aa46450cc33c79d152
Balisa16 <sanderantoniusbalisa16@gmail.com>
2024-02-13 20:35:14 +0700

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -33,11 +33,146 @@
 
 int width = 800, height = 800;
 
+struct Buffer
+{
+private:
+    const uint64_t data_limit = 600000;
+    uint64_t _size;
+
+public:
+    GLfloat *data;
+    Buffer() : data(new GLfloat[data_limit * 6]), _size(0) {}
+    void operator=(PCDFormat &new_data)
+    {
+        clear();
+        int __cnt = 0;
+        for (uint64_t i = 0; i < new_data.num_points; ++i)
+        {
+            if (__cnt >= data_limit)
+            {
+                _size = data_limit;
+                __cnt = 0;
+            }
+            data[__cnt * 6] = new_data.points[i].x;
+            data[__cnt * 6 + 1] = new_data.points[i].y;
+            data[__cnt * 6 + 2] = new_data.points[i].z;
+            data[__cnt * 6 + 3] = new_data.points[i].r;
+            data[__cnt * 6 + 4] = new_data.points[i].g;
+            data[__cnt * 6 + 5] = new_data.points[i].b;
+            __cnt++;
+        }
+        if (_size < data_limit)
+            _size = __cnt;
+    }
+
+    void operator=(Buffer &new_data)
+    {
+        clear();
+        int __cnt = 0;
+        for (uint64_t i = 0; i < new_data.size(); ++i)
+        {
+            if (__cnt >= data_limit)
+            {
+                _size = data_limit;
+                __cnt = 0;
+            }
+
+            data[__cnt * 6] = new_data.data[i * 6];
+            data[__cnt * 6 + 1] = new_data.data[i * 6 + 1];
+            data[__cnt * 6 + 2] = new_data.data[i * 6 + 2];
+            data[__cnt * 6 + 3] = new_data.data[i * 6 + 3];
+            data[__cnt * 6 + 4] = new_data.data[i * 6 + 4];
+            data[__cnt * 6 + 5] = new_data.data[i * 6 + 5];
+            __cnt++;
+        }
+
+        if (_size < data_limit)
+            _size = __cnt;
+    }
+
+    void operator+=(PCDFormat &new_data)
+    {
+        int __cnt = _size;
+        for (uint64_t i = 0; i < new_data.num_points; ++i)
+        {
+            if (__cnt >= data_limit)
+            {
+                _size = data_limit;
+                __cnt = 0;
+            }
+            data[(__cnt + i) * 6] = new_data.points[i].x;
+            data[(__cnt + i) * 6 + 1] = new_data.points[i].y;
+            data[(__cnt + i) * 6 + 2] = new_data.points[i].z;
+            data[(__cnt + i) * 6 + 3] = new_data.points[i].r;
+            data[(__cnt + i) * 6 + 4] = new_data.points[i].g;
+            data[(__cnt + i) * 6 + 5] = new_data.points[i].b;
+            __cnt++;
+        }
+        if (_size < data_limit)
+            _size = __cnt;
+    }
+
+    Buffer operator+(const Buffer &buffer)
+    {
+        Buffer result;
+        result = *this;
+
+        // Copy data from the second buffer
+        uint64_t __cnt = result._size;
+        for (uint64_t i = 0; i < buffer._size; ++i)
+        {
+            // Check if data too large
+            if (__cnt >= data_limit)
+            {
+                result._size = data_limit;
+                __cnt = 0;
+            }
+
+            result.data[(__cnt + i) * 6] = buffer.data[i * 6];
+            result.data[(__cnt + i) * 6 + 1] = buffer.data[i * 6 + 1];
+            result.data[(__cnt + i) * 6 + 2] = buffer.data[i * 6 + 2];
+            result.data[(__cnt + i) * 6 + 3] = buffer.data[i * 6 + 3];
+            result.data[(__cnt + i) * 6 + 4] = buffer.data[i * 6 + 4];
+            result.data[(__cnt + i) * 6 + 5] = buffer.data[i * 6 + 5];
+            __cnt++;
+        }
+
+        if (result._size < data_limit)
+            result._size = __cnt;
+
+        return result;
+    }
+
+    void clear()
+    {
+        if (data != nullptr)
+            delete[] data;
+
+        data = new GLfloat[data_limit * 6];
+        _size = 0;
+    }
+
+    uint64_t size() const
+    {
+        return _size;
+    }
+
+    GLfloat *get_data()
+    {
+        return data;
+    }
+};
+
 int main()
 {
     PCDReader parser("../sample/pointcloud1.pcd");
+    Buffer buff;
     // parser += "../sample/pointcloud3.pcd";
     PCDFormat data = parser.get_data();
+    buff = data;
+
+    for (uint64_t i = 0; i < buff.size(); ++i)
+        std::cout << i << '\t' << buff.data[i * 6] << " " << buff.data[i * 6 + 1] << " " << buff.data[i * 6 + 2] << std::endl;
 
     Window win("Point Cloud", width, height);
     GLFWwindow *window = win.get_window();
@@ -77,7 +212,7 @@ int main()
 
     // Main loop
 
-    int counter = 200;
+    int counter = 500;
     while (!glfwWindowShouldClose(window))
     {
         counter--;
